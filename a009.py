ffd77fd51bdbaeeacf06261db46f3b105499b298
dodo4910 <s916220@gmail.com>
2018-01-20 10:22:35 +0800

    add explain

--- a009.py
+++ a009.py
@@ -5,5 +5,6 @@ for text in sys.stdin:
     # 讀入文章
     for i in range(num):
         text1 += chr(ord(text[i])-7)
+        #在 python 裡面，字串是不允許變更，故需要重新創一空白string ，然後用加法的慢慢建立一個新的字串
 
     print(text1)
